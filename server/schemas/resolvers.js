a721720b075b2a1d53d4bd34556f6d81438f71a6
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-09-23 11:45:32 -0500

    Query: me, Mutation: addUser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,28 @@
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
 // Queries and Mutations for Mongoose models
 const resolvers = {
   Query: {
     testRun: () => {
       return "tada!!";
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id });
+        return userData;
+      }
+      console.log(context.user);
+      throw new AuthenticationError("You are not logged in !!!");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,24 @@ const { gql } = require("apollo-server-express");
 
 // Type Definitions
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     testRun: String
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
   }
 `;
 
