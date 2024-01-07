5d96af7b1009b59a7a10e5e664a002177c09df0c
Elizabeth Dieterich <dieterichelizabeth@gmail.com>
2024-01-07 13:24:31 -0600

    Updates Project for Render Deployment

---
+++ client/build/precache-manifest.be30be095ae110329de3638281c5de6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7472401199d60a1738cf1db50318c381",
+    "url": "/index.html"
+  },
+  {
+    "revision": "366cd38609b97b29033b",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "52b000b34dbb732e23a7",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "366cd38609b97b29033b",
+    "url": "/static/js/2.d4242ced.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.d4242ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52b000b34dbb732e23a7",
+    "url": "/static/js/main.0185ec6f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.0185ec6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -4,25 +4,23 @@ const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
 const path = require("path");
+const { InMemoryLRUCache } = require("@apollo/utils.keyvaluecache");
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
+  cache: new InMemoryLRUCache(),
 });
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 // Create an instance of an Apollo server with the GraphQL schema
